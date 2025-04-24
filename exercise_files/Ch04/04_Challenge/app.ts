ef83b50fb2102821474086f84a66c798af05ba10
edwinpav <edwinpavlovsky@gmail.com>
2025-04-24 15:04:46 -0400

    finish chapter 4

--- exercise_files/Ch04/04_Challenge/app.ts
+++ exercise_files/Ch04/04_Challenge/app.ts
@@ -1,6 +1,10 @@
 function query<T>(
     items: T[],
-    query: any // <--- replace this!
+    query: {[TProp in keyof T]?: (Function: T[TProp]) => boolean}
+    // other solution... (not fully correct)
+    // this results in each key's value having a type of either number or string
+    // bc T[keyof T] occurs for each key of T for a single key in the Record
+    // query: Record<keyof T, (Function: T[keyof T]) => boolean>
 ) {
     return items.filter(item => {
         // iterate through each of the item's properties
